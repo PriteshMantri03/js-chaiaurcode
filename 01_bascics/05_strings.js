7bce222bf18b44f405da18918a1c504e7466ccf9
PriteshMantri03 <priteshmantri053@gmail.com>
2025-09-23 17:52:17 +0000

    string

---
+++ 01_bascics/05_strings.js
@@ -0,0 +1,16 @@
+const name ="pritesh"
+const repoCOunt =50
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCOunt}`);
+
+const gameName = new String(`Pritesh-pm`)
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('t'));
+
+const newString =gameName.substring(0 ,4)
