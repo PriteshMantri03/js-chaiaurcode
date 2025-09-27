9f18c4f58a3aa5a636c59225eb6fd0caefe213a0
PriteshMantri03 <priteshmantri053@gmail.com>
2025-09-27 13:15:32 +0000

    objects1

--- 02_bascics/03_objects.js
+++ 02_bascics/03_objects.js
@@ -1,10 +1,14 @@
 // singleton
 
 // object literals
+const mySym = Symbol("key1");
+
+
 
 const JsUser={
     name: "Pritesh",
     "full name": "Pritesh Mantri",
+    [mySym]: "mykey1",
     age:20,
     location:"Pune",
     email: "pritesh@gmail.com",
@@ -12,6 +16,25 @@ const JsUser={
     lastLoginDays:["Monday", "Friday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email="hitesh@chatgpt.com";
+// Object.freeze(JsUser);
+JsUser.email="hitesh@microsoft.com";
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello js User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
