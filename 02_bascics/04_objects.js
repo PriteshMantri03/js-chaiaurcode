ef56f3c6dfbcc435bb3b62267b797fa3e1fe281a
PriteshMantri03 <priteshmantri053@gmail.com>
2025-09-27 13:37:11 +0000

    object2-1

---
+++ 02_bascics/04_objects.js
@@ -0,0 +1,22 @@
+// const tinderUser = new Object()
+const tinderUser={}
+
+tinderUser.id = "123abc"
+tinderUser.name ="Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email:"some@gmail.com",
+    fullname: {
+        Userfullname: {
+            firstname :"pritesh",
+            lastname:"mantri"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.Userfullname.firstname);
+
+const obj1 ={1: "a",2: "b"}
