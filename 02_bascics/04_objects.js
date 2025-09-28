43c4d1b9f41291647e74ba6c16b35fb8bc26b276
PriteshMantri03 <priteshmantri053@gmail.com>
2025-09-28 09:27:46 +0000

    objects

--- 02_bascics/04_objects.js
+++ 02_bascics/04_objects.js
@@ -20,3 +20,53 @@ const regularUser ={
 // console.log(regularUser.fullname.Userfullname.firstname);
 
 const obj1 ={1: "a",2: "b"}
+const obj2 ={3: "c",4: "d"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({} , obj1,obj2)
+
+// const obj3 ={...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email:"p@gmail.com"
+    },
+    {
+        id: 1,
+        email:"p@gmail.com"
+    },
+    {
+        id: 1,
+        email:"p@gmail.com"
+    },
+    
+]
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); 
+// console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+
+
+const course = {
+    courseanme: "js in hindi",
+    price :"666",
+    cousesrInstructor :"hitesh"
+}
+// course.cousesrInstructor
+
+// const {cousesrInstructor: instructor} =course
+// console.log(cousesrInstructor);
+// console.log(instructor);
+
+const { cousesrInstructor } = course   // normal destructuring
+console.log(cousesrInstructor);        // works
+
+const { cousesrInstructor: instructor } = course // aliasing
+console.log(instructor);                           // works
+
